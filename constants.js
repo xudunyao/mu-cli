51d14a89f516c8c18ba10eb68afdcde3f82272d4
xudunyao <xudunyao@126.com>
2026-03-31 15:55:05 +0800

    feat：命令行参数解析

---
+++ constants.js
@@ -0,0 +1,17 @@
+export const templates = [
+  {
+    name: 'webpack-template',
+    value: 'xudunyao/webpack-template',
+    desc: '基于webpack5的vue3项目模板'
+  },
+  {
+    name: 'vue-cli-template',
+    value: 'xudunyao/vue-cli-template',
+    desc: '基于vue-cli4的vue3项目模板'
+  },
+  {
+    name: 'vite-template',
+    value: 'xudunyao/vite-template',
+    desc: '基于vite的vue3 + 前端工具链项目模板'
+  }
+];
--- index.js
+++ index.js
@@ -1,6 +1,46 @@
+#!/usr/bin/env node
 import clone from './clone.js'
 import figlet from 'figlet';
 import chalk from 'chalk';
+import { program } from 'commander';
+import { templates } from './constants.js';
+
+program.version("0.0.1", "-v, --version", "输出当前版本号")
+
+program
+  .name("mu-cli")
+  .description("自定义脚手架")
+  .usage("<command> [options]")
+  .on('--help', () => {
+    console.log('\r\n' + chalk.greenBright.bold(figlet.textSync('mu-cli', {
+      font: 'Standard',
+      horizontalLayout: 'default',
+      verticalLayout: 'default',
+      width: 80,
+      whitespaceBreak: true
+    })));
+    console.log(`\r\nRun ${chalk.cyan(`mu-cli <command> --help`)} for detailed usage of given command\r\n`)
+})
+
+program
+  .command("create <app-name>")
+  .description("创建一个新项目")
+  .option("-f, --force", "强制创建")
+  .option("-t, --template <template>", "选择模板")
+  .action((name, options) => {
+    console.log(name)
+    console.log('options', options)
+  })
+
+program
+  .command("list")
+  .description("列出所有项目")
+  .action(() => {
+    console.log(chalk.yellowBright('模板列表'));
+    templates.forEach((temp, index) => {
+      console.log(`(${index + 1}) | ${temp.name} | ${temp.value} | ${temp.desc}`)
+    })
+  })
 
 console.log('\r\n' + chalk.greenBright.bold(figlet.textSync('mu-cli', {
   font: 'Standard',
@@ -10,4 +50,5 @@ console.log('\r\n' + chalk.greenBright.bold(figlet.textSync('mu-cli', {
   whitespaceBreak: true
 })));
 
-clone("xudunyao/learn-vue3-core", "demo")
+program.parse(process.argv)
+// clone("xudunyao/learn-vue3-core", "demo")
